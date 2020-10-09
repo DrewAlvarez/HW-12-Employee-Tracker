707177fe6a75e9467963252ddde52c20c3529f60
Andrew <alvarez.drew@gmail.com>
2020-10-09 12:58:45 -0500

    setting up files

---
+++ index.js
@@ -0,0 +1,16 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var consoleTable = require("console.table")
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employee_trackerDB"
+  });
+  
+  connection.connect(function(err) {
+    if (err) throw err;
+    start();
+  });
