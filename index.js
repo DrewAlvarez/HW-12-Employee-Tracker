8553a3287ce51039361bcef3db5d61175621fe60
Drew A <alvarez.drew@gmail.com>
2020-10-09 21:49:21 -0500

    set connection pw to grabbed through a json file that will be ignored

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var consoleTable = require("console.table")
+var config = require("./logs/config.json")
 
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
+    password: config.myPW,
     database: "employee_trackerDB"
   });
   
@@ -38,32 +39,33 @@ var connection = mysql.createConnection({
         name: "choice"
       }
     ]).then(function(startRes){
-      if(startRes.choice === "View All Employees"){
-        viewAllEmployees();
-      }else if(startRes.choice === "Add Employee"){
-        addEmployee();
-      }else if(startRes.choice === "Remove Employee"){
-        removeEmployee();
-      }else if(startRes.choice === "Update Employee Role"){
-        updateEmployeeRole();
-      }else if(startRes.choice === "Update Empoyee Manager"){
-        updateEmployeeManager();
-      }else if(startRes.choice === "Update Employee Department"){
-        updateEmployeeDepartment();
-      }else if(startRes.choice === "Add Role"){
-        addRole();
-      }else if(startRes.choice === "Remove Role"){
-        removeRole();
-      }else if(startRes.choice === "View Roles"){
-        viewRoles();
-      }else if(startRes.choice === "Add Department"){
-        addDepartment();
-      }else if(startRes.choice === "Remove Department"){
-        removeDepartment();
-      }else if(startRes.choice === "View Departments"){
-        viewDepartment();
-      }else{
-        connection.end();
-      }
+      console.log(startRes.choice)
+      // if(startRes.choice === "View All Employees"){
+      //   viewAllEmployees();
+      // }else if(startRes.choice === "Add Employee"){
+      //   addEmployee();
+      // }else if(startRes.choice === "Remove Employee"){
+      //   removeEmployee();
+      // }else if(startRes.choice === "Update Employee Role"){
+      //   updateEmployeeRole();
+      // }else if(startRes.choice === "Update Empoyee Manager"){
+      //   updateEmployeeManager();
+      // }else if(startRes.choice === "Update Employee Department"){
+      //   updateEmployeeDepartment();
+      // }else if(startRes.choice === "Add Role"){
+      //   addRole();
+      // }else if(startRes.choice === "Remove Role"){
+      //   removeRole();
+      // }else if(startRes.choice === "View Roles"){
+      //   viewRoles();
+      // }else if(startRes.choice === "Add Department"){
+      //   addDepartment();
+      // }else if(startRes.choice === "Remove Department"){
+      //   removeDepartment();
+      // }else if(startRes.choice === "View Departments"){
+      //   viewDepartment();
+      // }else{
+      //   connection.end();
+      // }
     })
   }
