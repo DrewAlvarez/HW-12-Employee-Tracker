1953a3f4e4c843a3cd6ce0ea8b0c05f77080a157
Drew A <alvarez.drew@gmail.com>
2020-10-09 21:30:56 -0500

    started index.js with inquirer prompt start function

--- index.js
+++ index.js
@@ -14,3 +14,56 @@ var connection = mysql.createConnection({
     if (err) throw err;
     start();
   });
+
+  function start() {
+    inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "Add Employee",
+          "Remove Employee",
+          "Update Employee Role",
+          "Update Employee Manager",
+          "Update Employee Department",
+          "Add Role",
+          "Remove Role",
+          "View Roles",
+          "Add Department",
+          "Remove Department",
+          "View Departments",
+          "EXIT"],
+        name: "choice"
+      }
+    ]).then(function(startRes){
+      if(startRes.choice === "View All Employees"){
+        viewAllEmployees();
+      }else if(startRes.choice === "Add Employee"){
+        addEmployee();
+      }else if(startRes.choice === "Remove Employee"){
+        removeEmployee();
+      }else if(startRes.choice === "Update Employee Role"){
+        updateEmployeeRole();
+      }else if(startRes.choice === "Update Empoyee Manager"){
+        updateEmployeeManager();
+      }else if(startRes.choice === "Update Employee Department"){
+        updateEmployeeDepartment();
+      }else if(startRes.choice === "Add Role"){
+        addRole();
+      }else if(startRes.choice === "Remove Role"){
+        removeRole();
+      }else if(startRes.choice === "View Roles"){
+        viewRoles();
+      }else if(startRes.choice === "Add Department"){
+        addDepartment();
+      }else if(startRes.choice === "Remove Department"){
+        removeDepartment();
+      }else if(startRes.choice === "View Departments"){
+        viewDepartment();
+      }else{
+        connection.end();
+      }
+    })
+  }
