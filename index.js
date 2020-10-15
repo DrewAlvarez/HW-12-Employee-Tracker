ebb24e5d799533e3550cd6e43348a82bc5bb5a00
Drew A <alvarez.drew@gmail.com>
2020-10-14 21:55:46 -0500

    removed code that referenced delete file

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table")
-const funcs = require("./funcs")
 require("dotenv").config();
 
 var connection = mysql.createConnection({
