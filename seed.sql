1570e4482f1377256f763bf2606c502b7b5ffa71
Drew A <alvarez.drew@gmail.com>
2020-10-14 21:53:56 -0500

    created schema.sql, to separate inserts, adn deleted a file.

--- seed.sql
+++ seed.sql
@@ -1,28 +1,3 @@
---Initialize the database with MYSQL
-DROP DATABASE IF EXISTS employee_trackerDB;
-CREATE DATABASE employee_trackerDB;
-USE employee_trackerDB;
-
-CREATE TABLE department(
-    id INT NOT NULL PRIMARY KEY,
-    name VARCHAR(30)
-);
-
-CREATE TABLE role(
-    id INT PRIMARY KEY,
-    title VARCHAR(30),
-    salary DECIMAL(10,2),
-    department_id INT
-);
-
-CREATE TABLE employee(
-    id INT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT NULL
-);
-
 --Some starter departments/roles/emplyoees should you wish to use them
 
 INSERT INTO department (name) VALUE ("Sales");
